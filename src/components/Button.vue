ba99007e25dddea243b9cda5a3e4b744c40a6296
gvitorlima <git624886@gmail.com>
2025-05-29 17:51:11 -0300

    feat: cuteness start

---
+++ src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button @click="testeApenas()" :style="{ background: color, color: fontColor }">
+    {{ text }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: #1d4ed8; /* azul mais suave */
+  color: white;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.375rem;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+}
+</style>
+
+<script>
+export default {
+  name: 'BonitaButton',
+  props: {
+    text: {
+      type: String,
+      default: 'BonitaButton',
+    },
+    color: {
+      type: String,
+    },
+    fontColor: {
+      type: String,
+      default: 'White',
+    },
+  },
+  methods: {
+    testeApenas() {
+      console.log('Clica baixo nengue !')
+    },
+  },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>Im the Header, Mue-he he he. {{ title }}</h1>
+    <BonitaButton color="#008080" text="Teal" />
+    <BonitaButton color="#7FFFD4" text="Aqua Marine" fontColor="black" />
+    <BonitaButton color="#9400D3" text="Dark Violet" />
+  </header>
+</template>
+
+<script>
+import BonitaButton from './Button.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    BonitaButton,
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Hueueue',
+    },
+  },
+}
+</script>
